de7f1ee055856fefadd530515b4721912c73ce9a
eQuantum <8466209+eq19@users.noreply.github.com>
2024-06-13 10:32:26 +0700

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-gcloud auth/credential_file_override activate-service-account project-owner@feedmapping.iam.gserviceaccount.com
+#gcloud auth/credential_file_override activate-service-account project-owner@feedmapping.iam.gserviceaccount.com
+gcloud config set account project-owner@feedmapping.iam.gserviceaccount.com
 gcloud info
